8b5517101a15ff5d095b20af528e6b8bca6f6815
mube99 <mubarekfaris9@gmail.com>
2022-04-27 17:43:41 +0100

    0x08. C - Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _print_rev_recursion - Prints a string in reverse
  * @s: string
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -12,5 +13,5 @@ void _print_rev_recursion(char *s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	} 
-    }
+	}
+}
